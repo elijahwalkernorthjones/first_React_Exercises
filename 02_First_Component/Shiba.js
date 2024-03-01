709494ae08f93d5538228bd08bd646def7cf14dd
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-03-01 11:15:58 -0500

    like 5 videos down, 90 to go, lol

--- 02_First_Component/Shiba.js
+++ 02_First_Component/Shiba.js
@@ -1,5 +1,25 @@
+
 const Shiba = () => {
   return <img src="https://66.media.tumblr.com/8844e689de079e6134709eba05acccd1/tumblr_ohtx6cUWOi1voqnhpo5_250.png" />
 }
 
-ReactDOM.render(<Shiba />, document.getElementById("root"))
+const Sharpaye = () => {
+  return <img src="https://www.akc.org/wp-content/uploads/2017/11/Chinese-Shar-Pei-standing-on-the-sidewalk.jpg" /> 
+}
+
+
+const App = () => {
+  return (
+      <div> 
+        <Shiba />
+        <Sharpaye />
+        <Shiba />
+      </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
